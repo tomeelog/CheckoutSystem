d2d51282c7b6b10b4ad0ec9a6275162b3198698c
Samuel Babalola <samuel@massless.io>
2024-11-17 18:35:43 +0000

    Test: Add failing test to verify GetTotalPrice returns 0 if no items are scanned

---
+++ CheckoutSystem.Test/Tests/CheckoutTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutSystem.Test.Tests
+{
+    [TestFixture]
+    public class CheckoutTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void GetTotalPrice_NoItemsScanned_ReturnsZero()
+        {
+            ICheckout checkout = new Checkout();
+            Assert.AreEqual(0, checkout.GetTotalPrice());
+        }
+    }
+}
