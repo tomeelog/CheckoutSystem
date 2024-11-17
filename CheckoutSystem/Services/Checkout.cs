601424347ffd69837903fd0021b5c15fbe04e3fe
Samuel Babalola <samuel@massless.io>
2024-11-17 18:52:19 +0000

    Implementation: Update Checkout class to use pricing rules and pass single item test

--- CheckoutSystem/Services/Checkout.cs
+++ CheckoutSystem/Services/Checkout.cs
@@ -10,15 +10,29 @@ namespace CheckoutSystem.Services
 {
     public class Checkout : ICheckout
     {
-        
+        private readonly Dictionary<string, int> _items = new();
+        private readonly Dictionary<string, PricingRule> _pricingRules;
+
+        public Checkout(Dictionary<string, PricingRule> pricingRules)
+        {
+            _pricingRules = pricingRules;
+        }
         public void Scan(string item)
         {
-           
+            if (_items.ContainsKey(item))
+                _items[item]++;
+            else
+                _items[item] = 1;
         }
 
         public int GetTotalPrice()
         {
-            return 0;
+            int totalPrice = 0;
+            foreach (var item in _items)
+            {
+                totalPrice += _pricingRules[item.Key].UnitPrice * item.Value;
+            }
+            return totalPrice;
         }
     }
 }
